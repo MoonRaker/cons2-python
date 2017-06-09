0c91ed86005121498d425fd9cc105eeb38972d17
MoonRaker <derek.groenendyk@gmail.com>
2017-06-09 13:01:10 -0700

    Updated conf.py

--- docs/sphnix/source/conf.py
+++ docs/sphnix/source/conf.py
@@ -66,8 +66,8 @@ extensions = [
     'sphinx.ext.extlinks',
     'numpydoc',
     'sphinx.ext.autosummary',
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
+    # 'IPython.sphinxext.ipython_directive',
+    # 'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
